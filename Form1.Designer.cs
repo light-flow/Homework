0dcaf9540bbe2e02702e07c9fd6bc5b3b3e1c8a6
light-flow <1830712901@qq.com>
2021-02-26 17:47:03 +0800

    第一次迭代

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,20 +29,260 @@ namespace HomeworkOne
         /// </summary>
         private void InitializeComponent()
         {
+            this.count = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.seven = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.one = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.zero = new System.Windows.Forms.Button();
+            this.left_bracket = new System.Windows.Forms.Button();
+            this.right_bracket = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.multplicate = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // count
+            // 
+            this.count.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.count.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.count.Location = new System.Drawing.Point(669, 407);
+            this.count.Name = "count";
+            this.count.Size = new System.Drawing.Size(75, 44);
+            this.count.TabIndex = 0;
+            this.count.Text = "计算";
+            this.count.UseVisualStyleBackColor = false;
+            this.count.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // eight
+            // 
+            this.eight.Location = new System.Drawing.Point(139, 107);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(59, 65);
+            this.eight.TabIndex = 1;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = true;
+            // 
+            // nine
+            // 
+            this.nine.Location = new System.Drawing.Point(253, 107);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(58, 65);
+            this.nine.TabIndex = 2;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = true;
+            // 
+            // seven
+            // 
+            this.seven.Location = new System.Drawing.Point(35, 107);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(63, 65);
+            this.seven.TabIndex = 3;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = true;
+            this.seven.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // four
+            // 
+            this.four.Location = new System.Drawing.Point(35, 213);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(63, 65);
+            this.four.TabIndex = 4;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = true;
+            // 
+            // five
+            // 
+            this.five.Location = new System.Drawing.Point(139, 213);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(59, 65);
+            this.five.TabIndex = 5;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = true;
+            // 
+            // six
+            // 
+            this.six.Location = new System.Drawing.Point(254, 213);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(57, 65);
+            this.six.TabIndex = 6;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = true;
+            // 
+            // one
+            // 
+            this.one.Location = new System.Drawing.Point(35, 314);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(63, 65);
+            this.one.TabIndex = 7;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = true;
+            // 
+            // two
+            // 
+            this.two.Location = new System.Drawing.Point(139, 314);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(59, 65);
+            this.two.TabIndex = 8;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = true;
+            // 
+            // three
+            // 
+            this.three.Location = new System.Drawing.Point(254, 314);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(57, 65);
+            this.three.TabIndex = 9;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 25);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // zero
+            // 
+            this.zero.Location = new System.Drawing.Point(35, 407);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(63, 65);
+            this.zero.TabIndex = 11;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // left_bracket
+            // 
+            this.left_bracket.Location = new System.Drawing.Point(139, 407);
+            this.left_bracket.Name = "left_bracket";
+            this.left_bracket.Size = new System.Drawing.Size(59, 61);
+            this.left_bracket.TabIndex = 12;
+            this.left_bracket.Text = "(";
+            this.left_bracket.UseVisualStyleBackColor = true;
+            // 
+            // right_bracket
+            // 
+            this.right_bracket.Location = new System.Drawing.Point(254, 407);
+            this.right_bracket.Name = "right_bracket";
+            this.right_bracket.Size = new System.Drawing.Size(57, 61);
+            this.right_bracket.TabIndex = 13;
+            this.right_bracket.Text = ")";
+            this.right_bracket.UseVisualStyleBackColor = true;
+            this.right_bracket.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(347, 107);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(58, 65);
+            this.add.TabIndex = 14;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(347, 213);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(58, 65);
+            this.minus.TabIndex = 15;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            // 
+            // multplicate
+            // 
+            this.multplicate.Location = new System.Drawing.Point(347, 314);
+            this.multplicate.Name = "multplicate";
+            this.multplicate.Size = new System.Drawing.Size(58, 65);
+            this.multplicate.TabIndex = 16;
+            this.multplicate.Text = "*";
+            this.multplicate.UseVisualStyleBackColor = true;
+            // 
+            // divide
+            // 
+            this.divide.Location = new System.Drawing.Point(347, 407);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(58, 61);
+            this.divide.TabIndex = 17;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(527, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 44);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "清零";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 480);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.multplicate);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.right_bracket);
+            this.Controls.Add(this.left_bracket);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.count);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "计算器";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button count;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button left_bracket;
+        private System.Windows.Forms.Button right_bracket;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button multplicate;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,56 @@ namespace HomeworkOne
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Equals("0")) {
+                textBox1.Text += zero.Text;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void append(Button button) {
+
+            if (!textBox1.Text.Equals("0"))
+            {
+                textBox1.Text += button.Text;
+            }
+            else {
+
+                textBox1.Text = button.Text;
+            }
+          
+        }
+
     }
+        
 }
